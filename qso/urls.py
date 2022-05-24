9aeb87e494d189c59a396781617c4e39ba14754a
John <flyboy1565@gmail.com>
2022-05-24 16:21:26 -0700

    ALL CRUD METHODS

--- qso/urls.py
+++ qso/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.ApiOverview, name="home"),
-    path("create/", views.add_qso, name="add-qso"),
-    # path()
+    path("create/", views.add_qso, name="create-qso"),
+    path("all/", views.list_qso, name="list-qso"),
+    path("update/<int:pk>/", views.update_qso, name="update-qso"),
+    path('qso/<int:pk/delete', views.delete_qso, name="delete-qso")
 ]
--- qso/views.py
+++ qso/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from os import stat
+from django.shortcuts import get_object_or_404, render
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -7,6 +8,7 @@ from rest_framework import serializers, status
 from .models import Qso
 from .api.serializers import QsoSerializer
 
+# api documentation for users
 @api_view(["GET"])
 def ApiOverview(request):
     api_urls = {
@@ -18,6 +20,7 @@ def ApiOverview(request):
     return Response(api_urls)
 
 
+# create
 @api_view(['POST'])
 def add_qso(request):
     qso = QsoSerializer(data=request.data)
@@ -27,3 +30,34 @@ def add_qso(request):
         qso.save()
         return Response(qso.data)
     return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+# list
+@api_view(['GET'])
+def list_qso(request):
+    if request.query_params:
+        qsos = Qso.objects.filter(**request.query_params.dict())
+    else:
+        qsos = Qso.objects.all()
+    if qsos:
+        data = QsoSerializer(qsos)
+        return Response(data)
+    return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+# update 
+@api_view(['POST'])
+def update_qso(request, pk):
+    qso = Qso.objects.get(pk=pk)
+    data = QsoSerializer(instance=qso,data=request.data)
+    if data.is_valid():
+        data.save()
+        return Response(data.data)
+    return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['DELETE'])
+def delete_qso(request, pk):
+    qso = get_object_or_404(Qso, pk=pk)
+    qso.delete()
+    return Response(status.HTTP_202_ACCEPTED)
